11193342709a340397da92456e2b79ad8bb05926
unknown <tharanga@quintsn.com>
2026-01-13 15:38:45 +0530

    Added the google tag script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SocialSidebar from "@/components/socialSidebar";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -89,6 +90,20 @@ export default function RootLayout({
         <link rel="icon" type="image/png" href="/img/logo_icon.png" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {/* Google Tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LHNW4SRSPF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-LHNW4SRSPF');
+          `}
+        </Script>
         <JsonLd data={organizationData} />
         {children}
 
